ad100ec831000c6d98e80f2c648271cc81f37a03
Bodescu Razvan <razvan-sebastian.bodescu@student.tuiasi.ro>
2022-03-15 19:12:02 +0200

    Proiect initial ( primul commit )

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+#SALUT PIP
+## proiect laborator 3 pip_proiect
+
+location: https://github.com/sebastian32322/pip_proiect_lab3
---
+++ src/SalutPIP/Afisare.java
@@ -0,0 +1,10 @@
+package SalutPIP;
+
+public class Afisare {
+
+	public static void main(String[] args) {
+		System.out.println("SALUT PIP (15.03.2022)");
+
+	}
+
+}
